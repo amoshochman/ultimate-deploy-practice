c5b395e5b719ffbeef2012169ef5c41b5c9d98bf
amoshochman <amoshochman1982@gmail.com>
2023-08-03 16:10:02 +0300

    removing space

--- application.py
+++ application.py
@@ -10,7 +10,6 @@ def index():
 def hello():
     return 'Hello World!'
 
-
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
